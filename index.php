9eb1a9e57e2ab6749a0797dd4b40bb84313a690f
Yash Vaidya <yashv@ithena.ai>
2025-03-14 15:09:18 -0400

    update page title from 'iSERV - Home' to 'Ithena Demos'

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>iSERV - Home</title>
+    <title>Ithena Demos</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
